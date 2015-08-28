b47948a3abe2c491f7867f4341d1442326336276
nate young <nhwilcox@gmail.com>
2015-08-28 10:12:57 -0700

    7 Store tests passing

--- src/Store.php
+++ src/Store.php
@@ -15,9 +15,57 @@
         {
             $this->store_name = (string) $new_store_name;
         }
+
         function getStoreName()
         {
             return $this->store_name;
         }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO stores (store_name) VALUES (
+                '{$this->getStoreName()}')");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_stores = $GLOBALS['DB']->query("SELECT * FROM stores;");
+            $stores = array();
+            foreach($returned_stores as $store) {
+                $store_name = $store['store_name'];
+                $id = $store['id'];
+                $new_store = new Store($store_name, $id);
+                array_push($stores, $new_store);
+            }
+            return $stores;
+        }
+
+        static function deleteAll()
+        {
+          $GLOBALS['DB']->exec("DELETE FROM stores;");
+        }
+
+        function update($new_store_name)
+        {
+            $GLOBALS['DB']->exec("UPDATE shoes SET store_name =
+                '{$new_store_name}' WHERE id = {$this->getId()};");
+            $this->setStoreName($new_store_name);
+        }
+
+        function delete()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stores WHERE
+                id = {$this->getId()};");
+            $GLOBALS['DB']->exec("DELETE FROM stores_brands WHERE
+                store_id = {$this->getId()};");
+        }
+
+
     }
-    ?>
+?>
--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -14,19 +14,21 @@
 
     class StoreTest extends PHPUnit_Framework_TestCase
     {
-        // protected function tearDown()
-        // {
-        //   Store::deleteAll();
-        //   Brand::deleteAll();
-        // }
+        protected function tearDown()
+        {
+          Store::deleteAll();
+          //Brand::deleteAll();
+        }
 
         function test_getStoreName()
         {
             //Arrange
             $store_name = "BIG FUCKING SHOE STORE";
             $test_Store = new Store($store_name);
+
             //Act
             $result = $test_Store->getStoreName();
+
             //Assert
             $this->assertEquals($store_name, $result);
         }
@@ -36,12 +38,101 @@
             //Arrange
             $store_name = "BIG FUCKING SHOE STORE";
             $test_store = new Store($store_name);
+
             //Act
             $test_store->setStoreName($store_name);
             $result = $test_store->getStoreName();
+
             //Assert
             $this->assertEquals("BIG FUCKING SHOE STORE", $result);
         }
+
+        function test_getId()
+        {
+            //Arrange
+            $store_name = "LOTSA SHOES HERE";
+            $id = 1;
+            $test_store = new Store($store_name, $id);
+
+            //Act
+            $result = $test_store->getId();
+
+            //Assert
+            $this->assertEquals(true, is_numeric($result));
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $store_name = "CLOTHES FOR YOUR FEET";
+            $id = 1;
+            $test_store = new Store($store_name, $id);
+
+            //Act
+            $test_store->save();
+
+            //Assert
+            $result = Store::getAll();
+            $this->assertEquals($test_store, $result[0]);
+        }
+
+        function test_update()
+        {
+            //Arrange
+            $store_name = "PLZ BUY SHOES HERE";
+            $id = 1;
+            $test_store = new Store($store_name, $id);
+            $test_store->save();
+            $new_store_name = "KOOL SHOES HERE";
+
+            //Act
+            $test_store->update($new_store_name);
+
+            //Assert
+            $this->assertEquals("KOOL SHOES HERE", $test_store->getStoreName());
+        }
+
+        function test_deleteStore()
+        {
+            //Arrange
+            $store_name = "CLOTHES FOR YOUR FEET";
+            $id = 1;
+            $test_store = new Store($store_name, $id);
+            $test_store->save();
+
+            $store_name2 = "John Franti and his Foot Fellowship";
+            $id2 = 2;
+            $test_store2 = new Store($store_name2, $id2);
+            $test_store2->save();
+
+            //Act
+            $test_store->delete();
+
+            //Assert
+            $this->assertEquals([$test_store2], Store::getAll());
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $store_name = "THE SHOE STORE";
+            $id = 1;
+            $test_store = new Store($store_name, $id);
+            $test_store->save();
+
+            $store_name2 = "THE BETTER SHOE STORE";
+            $id2 = 2;
+            $test_store2 = new Store($store_name2, $id2);
+            $test_store2->save();
+
+            //Act
+            $result = Store::getAll();
+
+            //Assert
+            $this->assertEquals([$test_store, $test_store2], $result);
+        }
+
+
 
     }
 
