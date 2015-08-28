11da2d7f05700de474513069a55a4a4347281840
nate young <nhwilcox@gmail.com>
2015-08-28 11:03:05 -0700

    adds 6 passing brand class tests

--- src/Brand.php
+++ src/Brand.php
@@ -26,6 +26,31 @@
             return $this->id;
         }
 
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO brands (brand_name) VALUES (
+                '{$this->getBrandName()}')");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_brands = $GLOBALS['DB']->query("SELECT * FROM brands;");
+            $brands = array();
+            foreach($returned_brands as $brand) {
+                $brand_name = $brand['brand_name'];
+                $id = $brand['id'];
+                $new_brand = new Brand($brand_name, $id);
+                array_push($brands, $new_brand);
+            }
+            return $brands;
+        }
+
+        static function deleteAll()
+        {
+          $GLOBALS['DB']->exec("DELETE FROM brands;");
+        }
+
 
     }
 
--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -18,7 +18,7 @@
         protected function tearDown()
         {
           Store::deleteAll();
-          //Brand::deleteAll();
+          Brand::deleteAll();
         }
 
         function test_getBrandName()
@@ -37,7 +37,7 @@
         function test_setBrandName()
         {
             //Arrange
-            $brand_name = "BIG FUCKING SHOE STORE";
+            $brand_name = "BRAH-DIDAS";
             $test_brand = new Brand($brand_name);
 
             //Act
@@ -45,7 +45,77 @@
             $result = $test_brand->getBrandName();
 
             //Assert
-            $this->assertEquals("BIG FUCKING SHOE STORE", $result);
+            $this->assertEquals("BRAH-DIDAS", $result);
+        }
+
+        function test_getId()
+        {
+            //Arrange
+            $brand_name = "FREEBOK";
+            $id = 1;
+            $test_brand = new Brand($brand_name, $id);
+
+            //Act
+            $result = $test_brand->getId();
+
+            //Assert
+            $this->assertEquals(true, is_numeric($result));
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $brand_name = "FREEBOK";
+            $id = 1;
+            $test_brand = new Brand($brand_name, $id);
+
+            //Act
+            $test_brand->save();
+
+            //Assert
+            $result = Brand::getAll();
+            $this->assertEquals($test_brand, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $brand_name = "LA SMEAR";
+            $id = 1;
+            $test_brand = new Brand($brand_name, $id);
+            $test_brand->save();
+
+            $brand_name2 = "HAIRWALK";
+            $id2 = 2;
+            $test_brand2 = new Brand($brand_name2, $id2);
+            $test_brand2->save();
+
+            //Act
+            $result = Brand::getAll();
+
+            //Assert
+            $this->assertEquals([$test_brand, $test_brand2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $brand_name = "BRAH-DIDAS";
+            $id = 1;
+            $test_brand = new Brand($brand_name, $id);
+            $test_brand->save();
+
+            $brand_name2 = "K-SWISS-CHEESE";
+            $id2 = 2;
+            $test_brand2 = new Brand($brand_name2, $id2);
+            $test_brand2->save();
+
+            //Act
+            Brand::deleteAll();
+
+            //Assert
+            $result = Brand::getAll();
+            $this->assertEquals([], $result);
         }
 
     }
