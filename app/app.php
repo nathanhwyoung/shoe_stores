b569708d0301d83729882109bf1eb4817fc932c6
nate young <nhwilcox@gmail.com>
2015-08-28 13:59:01 -0700

    adds partial app functionality

--- app/app.php
+++ app/app.php
@@ -19,6 +19,50 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
+
+    //GETS
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->get("/stores", function() use ($app) {
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+    });
+
+    $app->get("/brands", function() use ($app) {
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+    });
+
+
+    //POSTS
+
+    $app->post("/stores", function() use ($app) {
+        $store = new Store($_POST['store_name']);
+        $store->save();
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+    });
+
+    $app->post("/brands", function() use ($app) {
+        $brand = new Brand($_POST['brand_name']);
+        $brand->save();
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+    });
+
+
+    //EDIT GETS
+
+    $app->get("/stores/{id}", function($id) use ($app) {
+        $store = Store::find($id);
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getBrands(), 'all_brands' => Brand::getAll()));
+    });
+
+    $app->get("/brands/{id}", function($id) use ($app) {
+        $brand = Brand::find($id);
+        return $app['twig']->render('brand.html.twig', array('brand' => $brand, 'stores' => $brand->getStores(), 'all_stores' => Store::getAll()));
+    });
+
+
     return $app;
 
 ?>
